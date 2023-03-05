c4b24a60e40c9b9515143b1c2137306467de0425
Zerossik <zeross19940303@gmail.com>
2023-03-05 11:52:04 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function addGalleryEL() {
+  const result = galleryItems.map(el => {
+    return `<div class="gallery__item">
+      <a class="gallery__link" href="${el.original}">
+        <img
+          class="gallery__image"
+          src="${el.preview}"
+          data-source="${el.original}"
+          alt="${el.description}"
+        />
+      </a>
+    </div>;`;
+  });
+  return result;
+}
+console.log(addGalleryEL());
+{
+  /* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+}
