e8399c3b8ddd35e5e6409af70f8ea3b5c8e51909
Zerossik <zeross19940303@gmail.com>
2023-03-07 22:12:58 +0200

    task 2 add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+function addGalleryEL() {
+  const result = galleryItems
+    .map(el => {
+      return `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`;
+    })
+    .join(' ');
+  galleryEl.insertAdjacentHTML('afterbegin', result);
+  return;
+}
 
-console.log(galleryItems);
+function handleImgClick(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  evt.preventDefault();
+  createModalWindow(evt);
+  return;
+}
+
+function createModalWindow() {
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
+
+galleryEl.addEventListener('click', handleImgClick);
+addGalleryEL();
